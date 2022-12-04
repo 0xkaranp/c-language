0f72970e66ccbbe782aea4c047b7ce548c0f9fcd
0xkaranp <107405481+0xkaranp@users.noreply.github.com>
2022-12-04 16:21:22 +0530

    add,sub,mul,div

---
+++ tut2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int a,b
+    
+    printf("enter a number");
+    scanf("%d",a);
+
+    printf("enter a number");
+    scanf("%d,b");
+
+    printf("the addition is %d",a+b);
+    printf("the subtraction is %d",a-b);
+    printf("the multiplication is %d",a*b);
+    printf("the division is %d",a/b);
+
+ return 0;
+}
