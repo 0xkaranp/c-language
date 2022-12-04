eec6c96d3329839bf38acba2cc707cb12bf75980
0xkaranp <107405481+0xkaranp@users.noreply.github.com>
2022-12-04 15:54:56 +0530

    basic addition in c lang

---
+++ tut1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+
+{
+  int a,b;
+  printf("enter number for a")
+  scanf("%d",a;)
+ 
+  printf("enter a number for b")
+  scanf("%d",b;)
+ 
+  printf("the addition of numbers is")
+  scanf("%d",a+b)
+}
