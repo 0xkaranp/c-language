23f9a3adc15cd04c4c7aa337b1771a03061698af
0xkaranp <107405481+0xkaranp@users.noreply.github.com>
2022-12-09 23:33:41 +0530

    celcius to farenheit
    In this program i make a small program that converts celcius to farehenheit

---
+++ celcius to farenhite.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+
+{
+    float c,f;  //The doubt is why we have to declare to variable here,one variable is also enough na//
+    printf("Enter a temperature in degree celcius\n");
+    scanf("%f",&c);
+    f=9/5*c+32;
+    printf("the temperature in farenhite is %f",f);
+    
+    
+}
