7fa0f0180d0696a56ddd741d7540f0f189fe9fb6
0xkaranp <107405481+0xkaranp@users.noreply.github.com>
2022-12-16 20:42:25 +0530

    Llimit calculator

---
+++ limitcalculator.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int a,n;
+    printf("Enter a limit\n");
+    scanf("%d",&a);
+    a=0;
+    while(a<n){
+        printf("%d\t",a);        
+        a++;
+    }
+    return 0;
+}
