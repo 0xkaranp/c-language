653a950ab80d5a306cccd01efc9c370089213570
0xkaranp <107405481+0xkaranp@users.noreply.github.com>
2023-02-09 20:14:37 +0530

    factorial calculoator by recursive functions

---
+++ factbyrecurfunc.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int factorial(int num)
+{
+    if(num==1 || num==0)
+    {
+     return 1;
+    }
+     else{
+        return(num*factorial(num-1));
+     }
+
+     
+
+}
+int main()
+{   int num;
+    printf("Enter the number that you want factorail for");
+    scanf("%d",&num);
+    printf("The factorial of %d is %d",num,factorial(num));
+    return 0;
+
+}
